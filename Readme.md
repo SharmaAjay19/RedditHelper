857e3096ff575319675efe9a99cee82b8ef31789
Ajay Sharma <ajsharm@microsoft.com>
2019-12-25 18:32:19 -0800

    Readme and unsave all uptil the end

---
+++ Readme.md
@@ -0,0 +1,6 @@
+First steps:
+1. Create a reddit app by going to this url. https://www.reddit.com/prefs/apps . Preferred app type is script-app.
+2. Put in your username, password, client_id, client_secret (client credentials of the created app) in file Login.py
+
+To Unsave all saved posts on your account:
+Run UnsaveAll.py to unsave your saved posts. It will fetch saved posts in batches of 100 and sequentially unsave them.
--- UnsaveAll.py
+++ UnsaveAll.py
@@ -1,9 +1,12 @@
 from Login import reddit
 savedposts = list(reddit.user.me().saved(limit=None))
-print(f"Listed {len(savedposts)} saved posts.")
-for saved in savedposts:
-	try:
-		saved.unsave()
-		saved.clear_vote()
-	except ClientException as e:
-		print(f"{saved.permalink} failed to unsave due to {str(e)}")
+print(f"Fetched {len(savedposts)} saved posts.")
+while len(savedposts)>0:
+	for saved in savedposts:
+		try:
+			saved.unsave()
+			saved.clear_vote()
+		except ClientException as e:
+			print(f"{saved.permalink} failed to unsave due to {str(e)}")
+	savedposts = list(reddit.user.me().saved(limit=None))
+	print(f"Fetched {len(savedposts)} saved posts.")
